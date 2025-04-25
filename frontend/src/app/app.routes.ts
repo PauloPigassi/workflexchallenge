c5a17ac272752bf339c809eb0893eaf8be3dde61
ppigassi.fujitsu <ppigassi.fujitsu@toyota.com.br>
2025-04-25 19:07:07 -0300

    removing unused files

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { UserListComponent } from './user/views/user-list/user-list.component';
 import { WorkationListComponent } from './components/workation-list/workation-list.component';
 
 export const routes: Routes = [
