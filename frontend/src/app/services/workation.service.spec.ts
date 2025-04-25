fc4f08bd18c92c8ae24aaddc82b314af06470d3c
PauloPigassi <ppigassi@gmail.com>
2025-04-25 19:19:35 -0300

    Update workation.service.spec.ts

--- frontend/src/app/services/workation.service.spec.ts
+++ frontend/src/app/services/workation.service.spec.ts
@@ -1,16 +1,1 @@
-import { TestBed } from '@angular/core/testing';
 
-import { WorkationService } from './workation.service';
-
-describe('WorkationService', () => {
-  let service: WorkationService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(WorkationService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
