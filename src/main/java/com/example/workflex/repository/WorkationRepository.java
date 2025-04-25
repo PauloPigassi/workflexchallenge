3c20b006ab623c61ffee3991a9d65fe75c8eb059
ppigassi.fujitsu <ppigassi.fujitsu@toyota.com.br>
2025-04-25 17:32:27 -0300

    organization

--- src/main/java/com/example/workflex/repository/WorkationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.workflex.repository;
-
-import com.example.workflex.model.Workation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkationRepository extends JpaRepository<Workation, Long> {
-}
-
